2eaa173e1c54cb682bb37098c24d48088b137cba
Joshua Morey <josh@joshmorey.com>
2016-05-05 00:48:37 -0600

    Stubbed out dependency parsing, without inheritance

--- src/index.js
+++ src/index.js
@@ -2,23 +2,63 @@ import Squalus from './Squalus';
 import docReady from 'es6-docready';
 import yaml from 'js-yaml';
 
-function parse(types) {
-  const extendedSymbols = {
-    '&': '&&',
-  };
-  const symbols = [
-    '|',
-    '&',
-    '[',
-    '{',
-    '(',
-    ']',
-    '}',
-    ')',
-  ];
+// function toposort(sources, nameFunc, requiresFunc) {
+//   const edges = {};
+//   const s = [];
+//
+//   Object.keys(sources).forEach(name => {
+//     const source = sources[name];
+//     const requires = requiresFunc(source);
+//     if (requires && requires.length) {
+//       requires.forEach(dependency => {
+//         if (!sources[dependency]) {
+//           throw new Error(`Unknown dependency ${dependency}`);
+//         }
+//         edges[dependency] = edges[dependency] || [];
+//         edges[dependency].push(nameFunc(source));
+//       });
+//     } else {
+//       s.push(source);
+//     }
+//   });
+//
+//   let parents;
+//   const sorted = {};
+//   while (s.length > 0) {
+//     const nSource = s.pop();
+//     const n = nameFunc(nSource);
+//     sorted[n] = nSource;
+//     if (edges[n]) {
+//       parents = edges[n];
+//       while (parents.length > 0) {
+//         const m = parents.pop();
+//         const mSource = sources[m];
+//         const requires = requiresFunc(mSource);
+//         if (!requires || !requires.find(d => !sorted[d])) {
+//           s.push(mSource);
+//         }
+//       }
+//     }
+//   }
+//
+//   Object.keys(edges).forEach(name => {
+//     if (edges[name].length > 0) {
+//       throw new Error('Graph cycle; unable to sort');
+//     }
+//   });
+//
+//   return sorted;
+// }
 
+const register = {};
+
+function parse(types, topLevelType) {
   Object.keys(types).forEach(key => {
     let type = types[key];
+
+    if (!topLevelType) {
+      register[key] = new Set();
+    }
 
     if (typeof type === 'string') {
       type = type.trim();
@@ -30,49 +70,36 @@ function parse(types) {
       // scalar/predefined or inheritance or any
       // type
       // type1|type2|type3
-      // {type1 => type2}
+      // type1 => type2
       // int{1,2,7-9}?
       // string?[]?
       // content-type(image/png) && signature(89 50 4E 47 0D 0A 1A 0A)
 
-      const tokens = [];
-      let tokenStart = 0;
-      for (let i = 0; i < type.length; i++) {
-        const char = type.charAt(i);
-        let symbol = symbols[char];
-        if (symbol) {
-          if (extendedSymbols[char] && type.substr(i, extendedSymbols[char].length) === extendedSymbols[char]) {
-            if (i > 0) {
-              tokens.push(type.substring(tokenStart, i));
-            }
-            tokens.push(splitSymbols[char]);
-            i += (splitSymbols[char].length - 1);
-            tokenStart = i + 1;
+      const tokens = type.split(/([|{}()?]|&&|\[]|=>)/).map(t => t.trim()).filter(t => t !== '');
+
+      // remove ? [] {...}
+      const tokens2 = [];
+      for (let i = 0; i < tokens.length; i++) {
+        if (tokens[i] === '{') {
+          while (tokens[i] !== '}') {
+            ++i;
           }
-        } else if (i === type.length - 1) {
-          tokens.push(type.substring(tokenStart));
+          continue;
+        } else if (tokens[i] === '[]' || tokens[i] === '?' || tokens[i] === '=>' || tokens[i] === '|') {
+          continue;
         }
+        tokens2.push(tokens[i]);
       }
 
-      console.log(tokens.map(t => t.trim()).join(' '));
+      tokens2.forEach(t => register[topLevelType || key].add(t));
 
-      // const dependencies = new Map();
-      // const stack = [type];
-      //
-      // for (let i = 0; i < stack.length; i++) {
-      //   // any shortcut: string|int|float
-      //   if (type.indexOf('|') !== -1) {
-      //     type.split('|').forEach(t => stack.push(t));
-      //   }
-      // }
-      //
-      // dependencies.map(dep => {
-      //   if (dep.startsWith('{')) {
-      //
-      //   }
-      // });
+      console.log(tokens2.join(' '));
     } else {
-      // assume object
+      parse(type, topLevelType || key);
+    }
+
+    if (!topLevelType) {
+      console.log(register);
     }
   });
 }
