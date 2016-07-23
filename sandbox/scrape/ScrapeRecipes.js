ea8a5a2de7c69805bf8b94def1d21ff882a5de9f
Joshua Morey <josh@joshmorey.com>
2016-07-23 00:03:29 -0600

    finished summary scraper

--- sandbox/scrape/ScrapeRecipes.js
+++ sandbox/scrape/ScrapeRecipes.js
@@ -1,124 +1,111 @@
 /* eslint max-len: "off" */
 
 import rp from 'request-promise';
+import promiseRetry from 'promise-retry';
+import requestPromiseErrors from 'request-promise/errors';
 import cheerio from 'cheerio';
 import path from 'path';
 import fs from 'fs';
+
+const RequestError = requestPromiseErrors.RequestError;
 
 const rootDir = 'C:/tmp/scrape/recipes';
 const authHeader = 'Bearer P6k/U+2F1ECWIwpmI527pUM6CDKS71rBQLHc2r1GkZrkHgrwKadHn8zFifk+QeNlW7bMDibkfBj3mNVyFBaJ722BPud529q7ln0FSBlURFjzIm+JXu2boqr9QEv3yu6k';
 
 const categories = {
-  228: 'world-cuisine/australian-and-new-zealander',
-  // 96: 'salad',
-  // 86: 'world-cuisine',
-  // 94: 'soups-stews-and-chili',
-  // 80: 'main-dish',
+  76: 'appetizers-and-snacks',
+  77: 'drinks',
+  78: 'breakfast-and-brunch',
+  79: 'desserts',
+  80: 'main-dish',
+  81: 'side-dish',
+  82: 'trusted-brands-recipes-and-tips',
+  84: 'healthy-recipes',
+  85: 'holidays-and-events',
+  86: 'world-cuisine',
+  88: 'bbq-grilling',
+  92: 'meat-and-poultry',
+  93: 'seafood',
+  94: 'soups-stews-and-chili',
+  95: 'pasta-and-noodles',
+  96: 'salad',
+  236: 'us-recipes',
+  1116: 'fruits-and-vegetables',
+  1642: 'everyday-cooking',
+  17561: 'lunch',
+  17562: 'dinner',
+  17567: 'ingredients',
 };
 
-let items = [];
+// const onResolve = () => {
+//   if (!cards.length) {
+//     resolve(recipes);
+//   }
+//   return rp({
+//     uri: `http://allrecipes.com/recipe/${c.id}`,
+//     resolveWithFullResponse: true,
+//   }).then(response => {
+//     const $ = cheerio.load(response.body);
+//
+//     try {
+//       const ingredients = $("li.checkList__line span.recipe-ingred_txt[itemprop='ingredients']", '.recipe-ingredients')
+//         .contents().toArray().map(n => n.data);
+//       const directions = $("ol[itemProp='recipeInstructions'] span.recipe-directions__list--item")
+//         .contents().toArray().map(n => n.data);
+//       const calories = $('span.calorie-count').children().first().text();
+//       const prepTime = $("time[itemProp='prepTime']").attr('datetime');
+//       const totalTime = $("time[itemProp='totalTime']").attr('datetime');
+//
+//       recipes.push(Object.assign(recipe, {
+//         ingredients,
+//         directions,
+//         calories,
+//         prepTime: prepTime ? prepTime.substr(2) : null,
+//         totalTime: totalTime ? totalTime.substr(2) : null,
+//       }));
+//     } catch(e) {
+//       fs.writeFile(`c:/tmp/page-${c.id}.html`, response.body);
+//       // throw e;
+//     }
+//
+//     return Promise.resolve();
+//   }).then(() => {
+//     return rp({
+//       uri: `http://images.media-allrecipes.com/userphotos/600x600/${imageFile}`,
+//       resolveWithFullResponse: true,
+//       encoding: null,
+//     }).then(response => {
+//       fs.writeFile(path.join(rootDir, imageFile), response.body);
+//     });
+//   }).then(onResolve, reject);
+// };
 
-function getRecipes(categoryId, pageNumber) {
-  const recipes = [];
-
-  return rp({
-    uri: `https://apps.allrecipes.com/v1/assets/hub-feed?id=${categoryId}&pageNumber=${pageNumber}&isSponsored=true&sortType=p`,
-    headers: {
-      Authorization: authHeader,
-    },
-    resolveWithFullResponse: true,
-  }).then(response => {
-    return new Promise((resolve, reject) => {
-      const cards = JSON.parse(response.body).cards.filter(c => c.itemType === 'Recipe').reverse();
-
-      const onResolve = () => {
-        if (!cards.length) {
-          resolve(recipes);
-        }
-
-        const c = cards.pop();
-        const imageFile = c.imageUrl.split('/').pop();
-
-        process.stdout.write(`  ${recipes.length + items.length}\r`);
-
-        let recipe = {
-          id: c.id,
-          category: recipeCategories[categoryId],
-          title: c.title,
-          description: c.description,
-          cook: c.cook.displayName,
-          url: `http://allrecipes.com/recipe/${c.id}`,
-          image: imageFile,
-        };
-
-        return rp({
-          uri: `http://allrecipes.com/recipe/${c.id}`,
-          resolveWithFullResponse: true,
-        }).then(response => {
-          const $ = cheerio.load(response.body);
-
-          try {
-            const ingredients = $("li.checkList__line span.recipe-ingred_txt[itemprop='ingredients']", '.recipe-ingredients')
-              .contents().toArray().map(n => n.data);
-            const directions = $("ol[itemProp='recipeInstructions'] span.recipe-directions__list--item")
-              .contents().toArray().map(n => n.data);
-            const calories = $('span.calorie-count').children().first().text();
-            const prepTime = $("time[itemProp='prepTime']").attr('datetime');
-            const totalTime = $("time[itemProp='totalTime']").attr('datetime');
-
-            recipes.push(Object.assign(recipe, {
-              ingredients,
-              directions,
-              calories,
-              prepTime: prepTime ? prepTime.substr(2) : null,
-              totalTime: totalTime ? totalTime.substr(2) : null,
-            }));
-          } catch(e) {
-            fs.writeFile(`c:/tmp/page-${c.id}.html`, response.body);
-            // throw e;
+const rpr = req =>
+  promiseRetry(
+    retry =>
+      rp(req)
+        .catch(RequestError, e => {
+          if (e.cause.code === 'ECONNRESET' || e.cause.code === 'ETIMEDOUT') {
+            return retry(e);
           }
-
-          return Promise.resolve();
-        }).then(() => {
-          return rp({
-            uri: `http://images.media-allrecipes.com/userphotos/600x600/${imageFile}`,
-            resolveWithFullResponse: true,
-            encoding: null,
-          }).then(response => {
-            fs.writeFile(path.join(rootDir, imageFile), response.body);
-          });
-        }).then(onResolve, reject);
-      };
-
-      Promise.resolve().then(onResolve, reject);
-    });
-  }).then(recipes => {
-    recipes.forEach(r => items.push(r));
-    if (recipes.length == 0) {
-      const categoryIds = Object.keys(recipeCategories);
-      const index = categoryIds.indexOf(categoryId);
-      if (index === categoryIds.length - 1) {
-        // done
-      } else {
-        fs.writeFile(path.join(rootDir, `category-${categoryId}.json`), JSON.stringify(recipes, null, '  '));
-        items = [];
-
-        // next category
-        getRecipes(categoryIds[index + 1], 1);
-      }
-    } else {
-      getRecipes(categoryId, pageNumber + 1);
+          throw e;
+        }),
+    {
+      retries: 4,
+      minTimeout: 10,
+      maxTimeout: 1000,
+      randomize: true,
     }
-  });
-}
+  );
 
 function getPage(categoryId, pageNumber) {
   // console.log(`try page ${pageNumber}`);
-  return rp({
+  return rpr({
     uri: `https://apps.allrecipes.com/v1/assets/hub-feed?id=${categoryId}&pageNumber=${pageNumber}&isSponsored=true&sortType=p`,
     headers: {
       Authorization: authHeader,
     },
+    pool: { maxSockets: 10 },
   });
 }
 
@@ -162,7 +149,7 @@ function getItems(categoryId) {
   };
 
   return searchForLastPage(1).then(last => {
-    console.log(`last page ${last}`);
+    console.log(`[${categories[categoryId]}] ${last} pages`);
     return Promise.all(Array.from(new Array(last))
       .map((e, i) => i + 1)
       .filter(p => !skip.has(p))
@@ -175,8 +162,9 @@ function getItems(categoryId) {
     allCards.filter(c => c.itemType === 'Recipe').forEach(card => {
       uniqueCards.set(card.id, card);
     });
+    console.log(`[${categories[categoryId]}] ${uniqueCards.size} items`);
 
-    Promise.resolve(Array.from(uniqueCards.values()).map(card => ({
+    return Promise.resolve(Array.from(uniqueCards.values()).map(card => ({
       id: card.id,
       category: categories[categoryId],
       title: card.title,
@@ -188,12 +176,10 @@ function getItems(categoryId) {
 }
 
 export default function () {
-  // getRecipes(Object.keys(recipeCategories)[0], 1);
-
   return Promise.all(Object.keys(categories).map(categoryId =>
     getItems(categoryId)
   ))
     .then(items => {
-      console.log(items.length);
+      console.log(items.map(x => x.length).reduce((a, b) => a + b));
     });
 }
